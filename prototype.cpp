36ac70ccf14db29e4ef7464e9acb0b1a57e72447
martin <martin.sachin@gmail.com>
2019-12-09 12:54:21 +0100

    Updated to using use MeshFunction.

--- prototype.cpp
+++ prototype.cpp
@@ -1,61 +1,105 @@
 #include <raytracer/geometry/Mesh.h>
-#include <raytracer/physics/Functions.h>
+#include <raytracer/physics/MathFunctions.h>
 #include <raytracer/physics/Laser.h>
-#include <raytracer/geometry/Functions.h>
+#include <raytracer/geometry/GeometryFunctions.h>
 #include <raytracer/geometry/Ray.h>
+#include <raytracer/geometry/MeshFunction.h>
+#include <chrono>
 #include "mfem.hpp"
 
 using namespace raytracer::geometry;
 using namespace raytracer::physics;
 
-double density(const mfem::Vector &x){
-    return 12.8e20 * x(0);
+struct StopAtCritical {
+    StopAtCritical(const LaserRay &laserRay, const MeshFunction &density) :
+            laserRay(laserRay), density(density) {}
+
+    bool operator()(const Intersection &intersection) {
+        const auto element = *intersection.element;
+
+        auto currentDensity = density[element];
+        auto criticalDensity = laserRay.getCriticalDensity();
+        return currentDensity > criticalDensity.asDouble;
+    }
+
+private:
+    const LaserRay &laserRay;
+    const MeshFunction &density;
+};
+
+
+std::unique_ptr<Intersection> continueStraight(const Intersection &intersection) {
+    const auto element = intersection.element;
+    return findClosestIntersection(intersection.orientation, element->getFaces());
 }
 
 
+double density(const mfem::Vector &x) {
+    return 12.8e20 * x(0);
+}
+
+void absorbEnergyAtRayEnd(const LaserRay &laserRay, MeshFunction& absorbedEnergy) {
+    const auto &intersection = laserRay.intersections.back();
+    const auto element = *intersection.element;
+    absorbedEnergy[element] += laserRay.energy.asDouble;
+}
+
 int main(int argc, char *argv[]) {
+
+
+    //MFEM boilerplate -------------------------------------------------------------------------------------------------
+    bool visualization = true;
 
     DiscreteLine side{};
     side.segmentCount = 100;
     side.width = 1;
-    Mesh mesh(side, side);
+    auto mfemMesh = constructRectangleMesh(side, side);
 
-    mfem::Mesh externalMesh(
-            side.segmentCount,
-            side.segmentCount,
-            mfem::Element::Type::QUADRILATERAL,
-            true,
-            side.width,
-            side.width,
-            true);
+    mfem::L2_FECollection finiteElementCollection(0, 2);
+    mfem::FiniteElementSpace finiteElementSpace(mfemMesh.get(), &finiteElementCollection);
 
-    mfem::H1_FECollection finiteElementCollection(1, 2);
-    mfem::FiniteElementSpace finiteElementSpace(&externalMesh, &finiteElementCollection);
     mfem::GridFunction densityGridFunction(&finiteElementSpace);
     mfem::FunctionCoefficient densityFunctionCoefficient(density);
     densityGridFunction.ProjectCoefficient(densityFunctionCoefficient);
 
-    Laser laser(Length{1315e-7}, [](const Point &point) { return Vector(1, 0); }, Gaussian(0.2));
+    mfem::GridFunction absorbedEnergyGridFunction(&finiteElementSpace);
+    //End of MFEM boilerplate ------------------------------------------------------------------------------------------
 
-    std::vector<std::vector<Intersection>> rays;
-    for (const auto &laserRay : laser.generateRays(100, Point(-1, 0.1), Point(-1, 0.9))) {
-        Ray ray(HalfLine{laserRay.startPoint, laserRay.direction});
+    Mesh mesh(mfemMesh.get());
+    MeshFunction densityMeshFunction(densityGridFunction, finiteElementSpace);
+    MeshFunction absorbedEnergyMeshFunction(absorbedEnergyGridFunction, finiteElementSpace);
 
-        auto intersections = ray.findIntersections(
-                mesh,
-                [](const Intersection& intersection, const Element& element) {
-                    return findClosestIntersection(intersection.orientation, element.getFaces());
-                },
-                [&laserRay, &densityGridFunction](const Intersection& intersection, const Element& element) {
-                    mfem::Array<double> nodalValues;
-                    mfem::IntegrationPoint integrationPoint{};
-                    integrationPoint.Set2(intersection.orientation.point.x, intersection.orientation.point.y);
-                    auto currentDensity = densityGridFunction.GetValue(element.getId(), integrationPoint);
-                    auto criticalDensity = laserRay.getCriticalDensity().asDouble;
-                    return currentDensity > criticalDensity;
-                }
-        );
-        rays.emplace_back(intersections);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    Laser laser(
+            Length{1315e-7},
+            [](const Point &point) { return Vector(1, 0.1); },
+            Gaussian(0.2),
+            Point(-1, 0.3),
+            Point(-1, 0.7)
+    );
+
+    auto laserRays = laser.generateRays(10000);
+
+    for (auto &laserRay : laserRays) {
+        laserRay.generateIntersections(mesh, continueStraight, StopAtCritical(laserRay, densityMeshFunction));
     }
-    std::cout << "";
+
+    for (const auto &laserRay : laserRays) {
+        absorbEnergyAtRayEnd(laserRay, absorbedEnergyMeshFunction);
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto nanosecondDuration = std::chrono::duration_cast<std::chrono::nanoseconds>( end - start ).count();
+    auto duration = nanosecondDuration * 1e-9;
+
+    std::cout << "Execution took: " << duration << "s." << std::endl;
+
+
+    //GLVIS-------------------------------------------------------------------------------------------------------------
+    char vishost[] = "localhost";
+    int visport = 19916;
+    mfem::socketstream sol_sock(vishost, visport);
+    sol_sock.precision(8);
+    sol_sock << "solution\n" << *mfemMesh << absorbedEnergyGridFunction << std::flush;
 }
