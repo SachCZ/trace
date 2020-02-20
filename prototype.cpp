0c31ea44dafe889d07a0199a8d67f92217bcb066
martin <martin.sachin@gmail.com>
2020-02-20 23:59:10 +0100

    Moved functions to Raytracer (like GradientCalculator).

--- prototype.cpp
+++ prototype.cpp
@@ -4,163 +4,13 @@
 #include <raytracer/geometry/GeometryFunctions.h>
 #include <raytracer/geometry/Ray.h>
 #include <raytracer/geometry/MeshFunction.h>
+#include <raytracer/physics/LaserPropagation.h>
+#include <raytracer/physics/GradientCalculators.h>
 #include <chrono>
 #include "mfem.hpp"
 
 using namespace raytracer::geometry;
 using namespace raytracer::physics;
-
-struct StopAtCritical {
-    explicit StopAtCritical(const MeshFunction &density) :
-            density(density) {}
-
-    bool operator()(const Intersection &intersection, const LaserRay &laserRay) {
-        const auto element = *intersection.nextElement;
-
-        auto currentDensity = density.getValue(element);
-        auto criticalDensity = laserRay.getCriticalDensity();
-        return currentDensity > criticalDensity.asDouble;
-    }
-
-private:
-    const MeshFunction &density;
-};
-
-struct DontStop {
-    bool operator()(const Intersection&, const LaserRay&) {
-        return false;
-    }
-};
-
-
-std::unique_ptr<Intersection> continueStraight(const Intersection &intersection, const LaserRay &) {
-    return findClosestIntersection(intersection.orientation, intersection.nextElement->getFaces(), intersection.face);
-}
-
-class GradientCalculator {
-public:
-    virtual Vector getGradient(const Intersection &) const = 0;
-};
-
-class ConstantGradientCalculator : public GradientCalculator {
-public:
-    explicit ConstantGradientCalculator(const Vector &gradient) : gradient(gradient) {}
-
-    Vector getGradient(const Intersection &) const override {
-        return this->gradient;
-    }
-
-private:
-    const Vector gradient;
-};
-
-class H1GradientCalculator : public GradientCalculator {
-public:
-    H1GradientCalculator(mfem::FiniteElementSpace &l2Space, mfem::FiniteElementSpace &h1Space):
-            l2Space(l2Space), h1Space(h1Space), _density(&h1Space) {}
-
-    Vector getGradient(const Intersection &intersection) const override {
-        auto point = intersection.orientation.point;
-        auto previousGradient = this->getGradientAt(*intersection.previousElement, point);
-        auto nextGradient = this->getGradientAt(*intersection.nextElement, point);
-        return 0.5 * (previousGradient + nextGradient);
-    }
-
-    void updateDensity(mfem::GridFunction& density){
-        this->_density = convertH1toL2(density);
-    }
-
-private:
-    mfem::FiniteElementSpace &l2Space;
-    mfem::FiniteElementSpace &h1Space;
-    mfem::GridFunction _density;
-
-    Vector getGradientAt(const Element& element, const Point& point) const {
-        mfem::Vector result(2);
-        mfem::IntegrationPoint integrationPoint{};
-        integrationPoint.Set2(point.x, point.y);
-
-        auto transformation = this->h1Space.GetElementTransformation(element.id);
-        transformation->SetIntPoint(&integrationPoint);
-        this->_density.GetGradient(*transformation, result);
-
-        return {result[0], result[1]};
-    }
-
-    mfem::GridFunction convertH1toL2(const mfem::GridFunction &function) {
-        mfem::BilinearForm A(&h1Space);
-        A.AddDomainIntegrator(new mfem::MassIntegrator);
-        A.Assemble();
-        A.Finalize();
-
-        mfem::MixedBilinearForm B(&l2Space, &h1Space);
-        B.AddDomainIntegrator(new mfem::MassIntegrator);
-        B.Assemble();
-        B.Finalize();
-
-        mfem::LinearForm b(&h1Space);
-        B.Mult(function, b);
-
-        mfem::DSmoother smoother(A.SpMat());
-
-        mfem::GridFunction result(&h1Space);
-        result = 0;
-        mfem::PCG(A, smoother, b, result);
-        return result;
-    }
-};
-
-struct SnellsLaw {
-    explicit SnellsLaw(const MeshFunction &density, const GradientCalculator &gradientCalculator) :
-            density(density), gradientCalculator(gradientCalculator) {}
-
-    std::unique_ptr<Intersection> operator()(const Intersection &intersection, const LaserRay &laserRay) {
-        const auto previousElement = intersection.previousElement;
-        const auto nextElement = intersection.nextElement;
-
-        if (!previousElement) {
-            return findClosestIntersection(intersection.orientation, nextElement->getFaces(), intersection.face);
-        }
-
-        auto newIntersection = findClosestIntersection(
-                intersection.orientation,
-                nextElement->getFaces(),
-                intersection.face);
-        if (!newIntersection)
-            return newIntersection;
-        newIntersection->orientation.direction = getDirection(intersection, laserRay.getCriticalDensity());
-        return newIntersection;
-    }
-
-private:
-    const MeshFunction &density;
-    const GradientCalculator &gradientCalculator;
-
-    Vector getDirection(const Intersection &intersection, Density criticalDensity) {
-        const double n1 = std::sqrt(1 - density.getValue(*intersection.previousElement) / criticalDensity.asDouble);
-        const double n2 = std::sqrt(1 - density.getValue(*intersection.nextElement) / criticalDensity.asDouble);
-
-        const auto gradient = gradientCalculator.getGradient(intersection);
-        const auto &direction = intersection.orientation.direction;
-
-        const auto l = 1 / direction.getNorm() * direction;
-        auto n = 1 / gradient.getNorm() * gradient;
-        auto c = (-1) * n * l;
-        if (c < 0) {
-            c = -c;
-            n = (-1) * n;
-        }
-        const double r = n1 / n2;
-
-        auto root = 1 - r * r * (1 - c * c);
-        if (root > 0) {
-            return r * l + (r * c - std::sqrt(root)) * n;
-        } else {
-            return l + 2 * c * n;
-        }
-    }
-};
-
 
 double density(const mfem::Vector &x) {
     return 12.8e20 * x(0);
@@ -217,9 +67,9 @@ int main(int, char *[]) {
 
     Laser laser(
             Length{1315e-7},
-            [](const Point) { return Vector(1, 0.2); },
+            [](const Point) { return Vector(1, 1); },
             Gaussian(0.1),
-            Point(-0.1, 0.2),
+            Point(-0.1, 0.3),
             Point(-0.1, 0.1)
     );
 
--- scripts/plot_result.py
+++ scripts/plot_result.py
@@ -17,4 +17,5 @@ if __name__ == "__main__":
 
         plt.plot(x, y, "o-")
 
+    plt.axis('equal')
     plt.show()
