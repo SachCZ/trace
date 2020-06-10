ec6dfb827741b134424888aaaced58405595b0ee
martin <martin.sachin@gmail.com>
2020-06-10 15:18:01 +0200

    Moving LeastSquare to Raytracer.

--- scripts/vis_utils.py
+++ scripts/vis_utils.py
@@ -26,7 +26,10 @@ def read_vtk(filename):
 
     nodes = np.array(reader.GetOutput().GetPoints().GetData())
     nodes = nodes[:, :-1]
-    elements = np.array(reader.GetOutput().GetCells().GetData()).reshape((-1, 4))
+
+    points_count = reader.GetOutput().GetCell(0).GetNumberOfPoints()
+
+    elements = np.array(reader.GetOutput().GetCells().GetData()).reshape((-1, points_count + 1))
     elements = elements[:, 1:]
     return nodes, elements
 
@@ -47,3 +50,26 @@ def plot_grid_function(fig, axis, nodes, element_indexes, values):
     fig.colorbar(poly_collection, ax=axis)
     axis.add_collection(poly_collection)
     axis.autoscale()
+
+
+def read_gradient(filename):
+    data = np.genfromtxt(filename, delimiter=",")
+    x = data[:, 0]
+    y = data[:, 1]
+    grad_x = data[:, 2]
+    grad_y = data[:, 3]
+    return x, y, grad_x, grad_y
+
+
+def plot_quiver(axis, x, y, f_x, f_y, **kwargs):
+    u, v = np.meshgrid(f_x, f_y)
+    axis.quiver(x, y, u, v, **kwargs)
+
+def sample_analytic_gradient(x, y, function):
+    grad_x = []
+    grad_y = []
+    for x0, y0 in zip(x,y):
+        grad_x0, grad_y0 = function(x0, y0)
+        grad_x.append(grad_x0)
+        grad_y.append(grad_y0)
+    return np.array(grad_x), np.array(grad_y)
