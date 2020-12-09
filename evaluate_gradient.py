157a8962366cbcf6dc39927f84aaa190057e2f49
martin <martin.sachin@gmail.com>
2020-12-09 16:54:30 +0100

    backup

--- evaluate_gradient.py
+++ evaluate_gradient.py
@@ -1,14 +1,15 @@
+import os
+import sys
+
 import numpy as np
 import rayvis
 from matplotlib import pyplot as plt
-
-
-def analytic_fun(x, y):
-    return 3.69106 * np.cos(11.9066 * x + 9.36195 * y), 2.9022 * np.cos(11.9066 * x + 9.36195 * y)
+from matplotlib.gridspec import GridSpec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def calc_max_norm(field, analytic):
-    error = field - analytic(field.coord_x, field.coord_y)
+    error = field - analytic
     return max(error.norm())
 
 
@@ -18,37 +19,58 @@ def load_vector_fields(file_names):
             yield rayvis.read_vector_field(f)
 
 
-def calc_norms(vector_fields):
-    for vector_field in vector_fields:
-        yield calc_max_norm(vector_field, analytic_fun)
+def calc_norms(vector_fields, analytic_vector_fields):
+    for (vector_field, analytic_vector_field) in zip(vector_fields, analytic_vector_fields):
+        yield calc_max_norm(vector_field, analytic_vector_field)
 
 
-def main():
-    path = "run/gradient/output/household{}.msgpack"
+def plot_grad_summary(fig, dual_mesh, grid_function, gradient, analytic):
+    gs = GridSpec(nrows=2, ncols=2, width_ratios=[1, 1], height_ratios=[1, 1])
+    fun_axes = fig.add_subplot(gs[0, 0])
+    grad_axes = fig.add_subplot(gs[1, 0])
+    analytic_axes = fig.add_subplot(gs[0, 1])
+    diff_axes = fig.add_subplot(gs[1, 1])
+
+    contour = rayvis.plot_vector_field(grad_axes, gradient, dual_mesh)
+    fig.colorbar(contour, cax=make_axes_locatable(grad_axes).append_axes("right", size="5%", pad=0.05))
+
+    contour = rayvis.plot_grid_function(fun_axes, grid_function)
+    fig.colorbar(contour, cax=make_axes_locatable(fun_axes).append_axes("right", size="5%", pad=0.05))
+
+    error = gradient - analytic
+    grad_norm = np.asarray(2 * [gradient.norm()])
+    contour = rayvis.plot_vector_field(diff_axes, error, dual_mesh)
+    fig.colorbar(contour, cax=make_axes_locatable(diff_axes).append_axes("right", size="5%", pad=0.05))
+
+    contour = rayvis.plot_vector_field(analytic_axes, analytic, dual_mesh)
+    fig.colorbar(contour, cax=make_axes_locatable(analytic_axes).append_axes("right", size="5%", pad=0.05))
+
+
+def main(folder):
+    path = os.path.join(folder, "output/grad{}.msgpack")
+    analytic_path = os.path.join(folder, "output/analytic_grad{}.msgpack")
     segments_count = range(10, 101, 10)
     file_names = [path.format(count) for count in segments_count]
+    analytic_file_names = [analytic_path.format(count) for count in segments_count]
     vector_fields = list(load_vector_fields(file_names))
-    norms = calc_norms(vector_fields)
+    analytic_vector_fields = list(load_vector_fields(analytic_file_names))
+    norms = calc_norms(vector_fields, analytic_vector_fields)
     fig, axes = plt.subplots()
     h_eff = np.asarray(1) / segments_count
     axes.loglog(h_eff, list(norms), "o")
     axes.loglog(h_eff, h_eff ** 2)
 
-    fig, axes = plt.subplots()
-    contour = rayvis.plot_vector_field(axes, vector_fields[0])
-    color_bar = fig.colorbar(contour)
-    color_bar.ax.set_ylabel("$|\\vec{G}|$")
-
-    fig, axes = plt.subplots()
-    with open("run/gradient/output/household10.mfem") as f:
-        mesh = rayvis.read_mfem_mesh(f)
-    with open("run/gradient/output/household10.gf") as f:
-        gf = rayvis.read_grid_function(f, mesh)
-    contour = rayvis.plot_grid_function(axes, gf)
-    color_bar = fig.colorbar(contour)
-    color_bar.ax.set_ylabel("$|\\vec{G}|$")
-    plt.show()
+    for segments, vector_field, analytic_vector_field in zip(segments_count, vector_fields, analytic_vector_fields):
+        fig = plt.figure(figsize=(10, 5))
+        with open(os.path.join(folder, "output/mesh{}.mfem".format(segments))) as f:
+            mesh = rayvis.read_mfem_mesh(f)
+        with open(os.path.join(folder, "output/dual_mesh{}.mfem".format(segments))) as f:
+            dual_mesh = rayvis.read_mfem_mesh(f)
+        with open(os.path.join(folder, "output/func{}.gf".format(segments))) as f:
+            gf = rayvis.read_grid_function(f, mesh)
+        plot_grad_summary(fig, dual_mesh, gf, vector_field, analytic_vector_field)
+        plt.savefig(os.path.join(folder, "output/summary{}.png".format(segments)))
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
--- gradient.cpp
+++ gradient.cpp
@@ -1,31 +1,87 @@
 #include <raytracer.h>
+#include <yaml-cpp/yaml.h>
+#include <iostream>
 
-void gradientOnMesh(size_t segmentsCount, const std::function<double(raytracer::Point)>& analytic){
+using namespace raytracer;
+
+VectorField calcAnalyticGrad(const VectorField& discreteGradient, const std::function<Vector(Point)>& analyticGradFunc){
+    VectorField result;
+    for (const auto& pointAndVector : discreteGradient){
+        auto point = pointAndVector.first;
+        result[point] = analyticGradFunc(*point);
+    }
+    return result;
+}
+
+std::unique_ptr<std::ostream> openFile(const std::vector<std::string>& paths){
+    std::stringstream outputFilename;
+    for (const auto& path : paths){
+        outputFilename << path;
+    }
+    return std::make_unique<std::ofstream>(outputFilename.str());
+}
+
+void writeGradResult(
+        const std::string &basePath,
+        const std::string &fileId,
+        const MfemMesh &mesh,
+        const MfemMeshFunction &func,
+        const VectorField &gradient,
+        const VectorField &analyticGradient
+) {
     using namespace raytracer;
 
-    MfemMesh mesh(SegmentedLine{1.0, segmentsCount}, SegmentedLine{1.0, segmentsCount});
-    MfemL20Space space(mesh);
-    MfemMeshFunction func(space, analytic);
-    std::stringstream outputFilename;
-    outputFilename << "output/household" << segmentsCount << ".msgpack";
-    std::ofstream output(outputFilename.str());
-    auto gradient = calcHousGrad(mesh, func);
-    output << gradient;
-    std::stringstream functionOutputFilename;
-    functionOutputFilename << "output/household" << segmentsCount << ".gf";
-    std::ofstream functionOutput(functionOutputFilename.str());
-    functionOutput << func;
+    *openFile({basePath, "mesh", fileId, ".mfem"}) << mesh;
+    *openFile({basePath, "func", fileId, ".gf"}) << func;
+    *openFile({basePath, "grad", fileId, ".msgpack"}) << gradient;
+    *openFile({basePath, "analytic_grad", fileId, ".msgpack"}) << analyticGradient;
 
-    std::stringstream meshOutputFilename;
-    meshOutputFilename << "output/household" << segmentsCount << ".mfem";
-    std::ofstream meshOutput(meshOutputFilename.str());
-    meshOutput << mesh;
+    auto dualMeshOutput = openFile({basePath, "dual_mesh", fileId, ".mfem"});
+    writeDualMesh(*dualMeshOutput, mesh);
 }
 
-int main(int, char *[]) {
-    for (size_t count = 10; count <= 100; count += 10){
-        gradientOnMesh(count, [](const raytracer::Point& point){
-            return 1.62 + 0.31*std::sin(M_PI*(3.79*point.x + 2.98*point.y));
-        });
+int main(int, char *argv[]) {
+    YAML::Node config = YAML::LoadFile(argv[1]);
+
+    std::function<double(Point)> analyticFunc;
+    std::function<Vector(Point)> analyticGradFunc;
+    if (config["function"]) {
+        auto functionName = config["function"].as<std::string>();
+        if (functionName == "sin") {
+            using namespace std;
+            analyticFunc = [](const Point &point) {
+                return 1.62 + 0.31 * sin(M_PI * (3.79 * point.x + 2.98 * point.y));
+            };
+            analyticGradFunc = [](const Point &point) {
+                auto x = point.x;
+                auto y = point.y;
+                return Vector{3.69106 * cos(11.9066 * x + 9.36195 * y), 2.9022 * cos(11.9066 * x + 9.36195 * y)};
+            };
+        }
+    } else {
+        throw std::logic_error("'function' not defined in config file");
+    }
+
+    if (
+            config["meshes"]["segments_from"] &&
+            config["meshes"]["segments_to"] &&
+            config["meshes"]["segments_step"]
+            ) {
+        auto from = config["meshes"]["segments_from"].as<size_t>();
+        auto to = config["meshes"]["segments_to"].as<size_t>();
+        auto step = config["meshes"]["segments_step"].as<size_t>();
+        for (size_t count = from; count <= to; count += step) {
+            std::stringstream basePath;
+            MfemMesh mesh(SegmentedLine{1.0, count}, SegmentedLine{1.0, count});
+            MfemL20Space space(mesh);
+            MfemMeshFunction func(space, analyticFunc);
+            auto gradient = calcHousGrad(mesh, func);
+            auto analyticGradient = calcAnalyticGrad(gradient, analyticGradFunc);
+            writeGradResult("output/", std::to_string(count), mesh, func, gradient, analyticGradient);
+        }
+    } else {
+        throw std::logic_error("meshes have invalid config in config file");
     }
 }
+
+
